e654540c08cf9612f9f6800b995ea992567b61f0
Ismo Torvinen <ismo.torvinen@gmail.com>
2016-04-28 20:33:52 +0300

    mac: relative path tehty

---
+++ oNet.cpp
@@ -0,0 +1,9 @@
+//
+//  oNet.cpp
+//  opengltest
+//
+//  Created by Ismo Torvinen on 28.4.2016.
+//  Copyright (c) 2016 Ismo Torvinen. All rights reserved.
+//
+
+#include <stdio.h>
---
+++ oNet.h
@@ -0,0 +1,13 @@
+//
+//  oNet.h
+//  opengltest
+//
+//  Created by Ismo Torvinen on 28.4.2016.
+//  Copyright (c) 2016 Ismo Torvinen. All rights reserved.
+//
+
+#ifndef opengltest_oNet_h
+#define opengltest_oNet_h
+
+
+#endif
